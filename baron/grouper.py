06b54ec84830893c04265423b6017b8c55e64a12
Laurent Peuch <cortex@worlddomination.be>
2013-03-25 00:31:06 +0100

    [enh] code the grouper

---
+++ baron/grouper.py
@@ -0,0 +1,53 @@
+# encoding: utf-8
+
+import re
+from utils import FlexibleIterator
+
+to_group = (
+    ("+", "="),
+    ("-", "="),
+    ("*", "="),
+    ("/", "="),
+    ("%", "="),
+    ("&", "="),
+    ("|", "="),
+    ("^", "="),
+    ("/", "/"),
+    ("*", "*"),
+    ("<", "<"),
+    (">", ">"),
+    ("=", "="),
+    ("!", "="),
+    ("<", ">"),
+    ("**", "="),
+    ("//", "="),
+    ("<<", "="),
+    (">>", "="),
+)
+
+to_group_keys, _ = zip(*to_group)
+
+
+def group(sequence):
+    return list(group_generator(sequence))
+
+
+def group_generator(sequence):
+    iterator = FlexibleIterator(sequence)
+    current = None
+    while True:
+        if iterator.end():
+            return
+
+        current = iterator.next()
+        if current in to_group_keys and matching_found(to_group, current, iterator.show_next()):
+            current += iterator.next()
+        if current in to_group_keys and matching_found(to_group, current, iterator.show_next()):
+            current += iterator.next()
+        if current == "@" and re.match("[a-zA-Z_]+", iterator.show_next()):
+            current += iterator.next()
+        yield current
+
+
+def matching_found(to_group, current, target):
+    return target in zip(*filter(lambda x: x[0] == current, to_group))[1]
---
+++ baron/test_grouper.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+from grouper import group
+
+def test_empty():
+    assert group([]) == []
+
+def test_one():
+    assert group(['a']) == ['a']
+
+def test_random():
+    assert group(list("abcdef")) == list("abcdef")
+
+def test_add_egual():
+    assert group(["+", "="]) == ["+="]
+
+def test_add_add():
+    assert group(["+", "+"]) == ["+", "+"]
+
+def test_add_egual_double():
+    assert group(["+", "=", "+", "="]) == ["+=", "+="]
+
+def test_add_egual_random():
+    assert group(list(" qsd += qsd")) == [" ", "q", "s", "d", " ", "+=", " ", "q", "s", "d"]
+
+def test_minus_egual():
+    assert group(["-", "="]) == ["-="]
+
+def test_mult_egual():
+    assert group(["*", "="]) == ["*="]
+
+def test_div_egual():
+    assert group(["/", "="]) == ["/="]
+
+def test_modulo_egual():
+    assert group(["%", "="]) == ["%="]
+
+def test_amper_egual():
+    assert group(["&", "="]) == ["&="]
+
+def test_bar_egual():
+    assert group(["|", "="]) == ["|="]
+
+def test_power_egual():
+    assert group(["^", "="]) == ["^="]
+
+def test_less_less():
+    assert group(["<", "<"]) == ["<<"]
+
+def test_more_more():
+    assert group([">", ">"]) == [">>"]
+
+def test_egual_egual():
+    assert group(["=", "="]) == ["=="]
+
+def test_different():
+    assert group(["!", "="]) == ["!="]
+
+def test_different_old_style():
+    assert group(["<", ">"]) == ["<>"]
+
+def test_power_power_egual():
+    assert group(["*", "*", "="]) == ["**="]
+
+def test_div_div_egual():
+    assert group(["/", "/", "="]) == ["//="]
+
+def test_less_less_egual():
+    assert group(["<", "<", "="]) == ["<<="]
+
+def test_more_more_egual():
+    assert group([">", ">", "="]) == [">>="]
+
+def test_decorator():
+    assert group(["@", "pouet"]) == ["@pouet"]
