2b6327f51794c4d91590fd58b196cde1c4fa7ddc
Laurent Peuch <cortex@worlddomination.be>
2013-04-02 03:07:13 +0200

    [enh] tokenize \o/

---
+++ baron/test_tokenizer.py
@@ -0,0 +1,273 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+
+from tokenizer import tokenize, KEYWORDS
+
+def match(string, token):
+    assert tokenize([string]) == [(token, string), None]
+
+def test_empty():
+    assert tokenize([]) == [None]
+
+def test_name():
+    match('a', 'NAME')
+    match('pouet', 'NAME')
+
+def test_name__():
+    match('_a', 'NAME')
+
+def test_name_number():
+    match('a123', 'NAME')
+
+def test_number():
+    match('1234', 'INT')
+    match('1234L', 'INT')
+    match('1234l', 'INT')
+
+def test_float():
+    match('1234.', 'FLOAT')
+    match('.1234', 'FLOAT')
+    match('1234.1234', 'FLOAT')
+    match('1234.L', 'FLOAT')
+    match('.1234L', 'FLOAT')
+    match('1234.1234L', 'FLOAT')
+    match('1234.l', 'FLOAT')
+    match('.1234l', 'FLOAT')
+    match('1234.1234l', 'FLOAT')
+
+def test_hexa():
+    match('0x123ABCD', 'HEXA')
+    match('0X123ABCD', 'HEXA')
+    match('0x123ABCDl', 'HEXA')
+    match('0x123ABCDL', 'HEXA')
+
+def test_octa():
+    match('012345', 'OCTA')
+    match('0o12345', 'OCTA')
+    match('0O1235', 'OCTA')
+    match('0O1235l', 'OCTA')
+    match('0O1235L', 'OCTA')
+
+def test_float_exponant():
+    match('1234e10', 'FLOAT_EXPONANT')
+    match('1234E10', 'FLOAT_EXPONANT')
+    match('1234e+10', 'FLOAT_EXPONANT')
+    match('1234e-10', 'FLOAT_EXPONANT')
+    match('1234E+10', 'FLOAT_EXPONANT')
+    match('1234E-10', 'FLOAT_EXPONANT')
+    match('1234.5678e10', 'FLOAT_EXPONANT')
+    match('1234.5678E10', 'FLOAT_EXPONANT')
+    match('1234.5678e+10', 'FLOAT_EXPONANT')
+    match('1234.5678e-10', 'FLOAT_EXPONANT')
+    match('1234.5678E+10', 'FLOAT_EXPONANT')
+    match('1234.5678E-10', 'FLOAT_EXPONANT')
+    match('.5678e10', 'FLOAT_EXPONANT')
+    match('.5678E10', 'FLOAT_EXPONANT')
+    match('.5678e+10', 'FLOAT_EXPONANT')
+    match('.5678e-10', 'FLOAT_EXPONANT')
+    match('.5678E+10', 'FLOAT_EXPONANT')
+    match('.5678E-10', 'FLOAT_EXPONANT')
+
+def test_left_parenthesis():
+    match('(', 'LEFT_PARENTHESIS')
+
+def test_right_parenthesis():
+    match(')', 'RIGHT_PARENTHESIS')
+
+def test_colon():
+    match(':', 'COLON')
+    assert tokenize([':']) == [('COLON', ':'), None]
+
+def test_comma():
+    match(',', 'COMMA')
+
+def test_semicolon():
+    match(';', 'SEMICOLON')
+
+def test_sequence():
+    assert tokenize(['a', '123']) == [('NAME', 'a'), ('INT', '123'), None]
+
+def test_plus():
+    match('+', 'PLUS')
+
+def test_minus():
+    match('-', 'MINUS')
+
+def test_start():
+    match('*', 'STAR')
+
+def test_slash():
+    match('/', 'SLASH')
+
+def test_vbar():
+    match('|', 'VBAR')
+
+def test_amper():
+    match('&', 'AMPER')
+
+def test_less():
+    match('<', 'LESS')
+
+def test_greater():
+    match('>', 'GREATER')
+
+def test_equal():
+    match('=', 'EQUAL')
+
+def test_dot():
+    match('.', 'DOT')
+
+def test_percent():
+    match('%', 'PERCENT')
+
+def test_left_square_bracket():
+    match('[', 'LEFT_SQUARE_BRACKET')
+
+def test_right_square_bracket():
+    match(']', 'RIGHT_SQUARE_BRACKET')
+
+def test_back_quote():
+    match('`', 'BACKQUOTE')
+
+def test_equal_equal():
+    match('==', 'EQUAL_EQUAL')
+
+def test_not_equal():
+    match('!=', 'NOT_EQUAL')
+
+def test_less_equal():
+    match('<=', 'LESS_EQUAL')
+
+def test_greater_equal():
+    match('>=', 'GREATER_EQUAL')
+
+def test_tilde():
+    match('~', 'TILDE')
+
+def test_circumflex():
+    match('^', 'CIRCUMFLEX')
+
+def test_left_shift():
+    match('<<', 'LEFT_SHIFT')
+
+def test_right_shift():
+    match('>>', 'RIGHT_SHIFT')
+
+def test_double_star():
+    match('**', 'DOUBLE_STAR')
+
+def test_plus_equal():
+    match('+=', 'PLUS_EQUAL')
+
+def test_minus_equal():
+    match('-=', 'MINUS_EQUAL')
+
+def test_star_equal():
+    match('*=', 'STAR_EQUAL')
+
+def test_slash_equal():
+    match('/=', 'SLASH_EQUAL')
+
+def test_percent_equal():
+    match('%=', 'PERCENT_EQUAL')
+
+def test_amper_equal():
+    match('&=', 'AMPER_EQUAL')
+
+def test_vbar_equal():
+    match('|=', 'VBAR_EQUAL')
+
+def test_circumflex_equal():
+    match('^=', 'CIRCUMFLEX_EQUAL')
+
+def test_left_shift_equal():
+    match('<<=', 'LEFT_SHIFT_EQUAL')
+
+def test_right_shift_equal():
+    match('>>=', 'RIGHT_SHIFT_EQUAL')
+
+def test_double_star_equal():
+    match('**=', 'DOUBLE_STAR_EQUAL')
+
+def test_double_slash():
+    match('//', 'DOUBLE_SLASH')
+
+def test_double_slash_equal():
+    match('//=', 'DOUBLE_SLASH_EQUAL')
+
+def test_keywords():
+    for keyword in KEYWORDS:
+        assert tokenize([keyword]) == [(keyword.upper(), keyword), None]
+
+def test_endl():
+    match('\n', 'ENDL')
+    match('\r\n', 'ENDL')
+
+def test_simple_string():
+    match('"pouet pouet"', 'STRING')
+    match("'pouet pouet'", "STRING")
+
+def test_multi_string():
+    match('"""pouet pouet"""', 'STRING')
+    match("'''pouet pouet'''", "STRING")
+
+def test_unicode_string():
+    match('u"pouet pouet"', 'UNICODE_STRING')
+    match("u'pouet pouet'", "UNICODE_STRING")
+    match('u"""pouet pouet"""', 'UNICODE_STRING')
+    match("u'''pouet pouet'''", "UNICODE_STRING")
+    match('U"pouet pouet"', 'UNICODE_STRING')
+    match("U'pouet pouet'", "UNICODE_STRING")
+    match('U"""pouet pouet"""', 'UNICODE_STRING')
+    match("U'''pouet pouet'''", "UNICODE_STRING")
+
+def test_raw_string():
+    match('r"pouet pouet"', 'RAW_STRING')
+    match("r'pouet pouet'", "RAW_STRING")
+    match('r"""pouet pouet"""', 'RAW_STRING')
+    match("r'''pouet pouet'''", "RAW_STRING")
+    match('R"pouet pouet"', 'RAW_STRING')
+    match("R'pouet pouet'", "RAW_STRING")
+    match('R"""pouet pouet"""', 'RAW_STRING')
+    match("R'''pouet pouet'''", "RAW_STRING")
+
+def test_binary_string():
+    match('b"pouet pouet"', 'BINARY_STRING')
+    match("b'pouet pouet'", "BINARY_STRING")
+    match('b"""pouet pouet"""', 'BINARY_STRING')
+    match("b'''pouet pouet'''", "BINARY_STRING")
+    match('B"pouet pouet"', 'BINARY_STRING')
+    match("B'pouet pouet'", "BINARY_STRING")
+    match('B"""pouet pouet"""', 'BINARY_STRING')
+    match("B'''pouet pouet'''", "BINARY_STRING")
+
+def test_raw_unicode_string():
+    match('ur"pouet pouet"', 'UNICODE_RAW_STRING')
+    match("uR'pouet pouet'", "UNICODE_RAW_STRING")
+    match('uR"""pouet pouet"""', 'UNICODE_RAW_STRING')
+    match("ur'''pouet pouet'''", "UNICODE_RAW_STRING")
+    match('Ur"pouet pouet"', 'UNICODE_RAW_STRING')
+    match("UR'pouet pouet'", "UNICODE_RAW_STRING")
+    match('UR"""pouet pouet"""', 'UNICODE_RAW_STRING')
+    match("Ur'''pouet pouet'''", "UNICODE_RAW_STRING")
+
+def test_raw_binary_string():
+    match('br"pouet pouet"', 'BINARY_RAW_STRING')
+    match("bR'pouet pouet'", "BINARY_RAW_STRING")
+    match('bR"""pouet pouet"""', 'BINARY_RAW_STRING')
+    match("br'''pouet pouet'''", "BINARY_RAW_STRING")
+    match('Br"pouet pouet"', 'BINARY_RAW_STRING')
+    match("BR'pouet pouet'", "BINARY_RAW_STRING")
+    match('BR"""pouet pouet"""', 'BINARY_RAW_STRING')
+    match("Br'''pouet pouet'''", "BINARY_RAW_STRING")
+
+def test_comment():
+    match('# pouet pouet', 'COMMENT')
+
+def test_space():
+    match(' ', 'SPACE')
+    match('	', 'SPACE')
+    match('	 ', 'SPACE')
+    match('	 \\\n ', 'SPACE')
+    match('	 \\\r\n ', 'SPACE')
