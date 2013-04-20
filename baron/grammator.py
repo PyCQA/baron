5df9a2f9301aae91e1cee4b290648839a9b68648
Laurent Peuch <cortex@worlddomination.be>
2013-04-21 00:48:19 +0200

    [mod] rename variable for more lisibility

--- baron/grammator.py
+++ baron/grammator.py
@@ -540,149 +540,149 @@ def create_node(name, value, **kwargs):
     return result
 
 @pg.production("main : statements")
-def main(p):
-    return filter(None, p[0]) if p[0] else []
+def main((statements,)):
+    return filter(None, statements) if statements else []
 
 @pg.production("statements : statements statement")
-def statements_statement(p):
-    return p[0] + p[1]
+def statements_statement((statements, statement)):
+    return statements + statement
 
 @pg.production("statements : statement")
-def statement(p):
-    return p[0]
+def statement((statement,)):
+    return statement
 
 @pg.production("statement : ENDMARKER")
-def end(p):
+def end(_):
     return [None]
 
 @pg.production("statement : expression")
-def exprs(p):
-    return [create_node("expression", p[0])]
+def exprs((expression,)):
+    return [create_node("expression", expression)]
 
 @pg.production("statement : separator")
 @pg.production("statement : import")
 @pg.production("statement : from_import")
-def separator(p):
-    return [p[0]]
+def separator((statement,)):
+    return [statement]
 
 @pg.production("expression : INT")
-def int(p):
-    return create_node_from_token(p[0], section="number")
+def int((int_,)):
+    return create_node_from_token(int_, section="number")
 
 @pg.production("expression : NAME")
-def name(p):
-    return create_node_from_token(p[0])
+def name((name,)):
+    return create_node_from_token(name)
 
 @pg.production("expression : STRING")
-def string(p):
-    return create_node_from_token(p[0])
+def string((string_,)):
+    return create_node_from_token(string_)
 
 @pg.production("separator : SPACE ENDL")
-def space_endl(p):
-    return {"type": p[1].name.lower(), "value": p[1].value, "before_space": p[0].value}
+def space_endl((space, endl,)):
+    return {"type": endl.name.lower(), "value": endl.value, "before_space": space.value}
 
 @pg.production("separator : ENDL")
-def endl(p):
-    return {"type": "endl", "value": p[0].value, "before_space": ""}
+def endl((endl,)):
+    return {"type": "endl", "value": endl.value, "before_space": ""}
 
 @pg.production("separator : SPACE ENDMARKER")
-def space(p):
-    return create_node_from_token(p[0])
+def space((space, endmarker)):
+    return create_node_from_token(space)
 
 @pg.production("import : IMPORT SPACE dotted_as_names")
-def importeu(p):
-    return {"type": "import", "value": p[2], "space": p[1].value}
+def importeu((import_, space, dotted_as_names)):
+    return {"type": "import", "value": dotted_as_names, "space": space.value}
 
 @pg.production("from_import : FROM from_import_module IMPORT from_import_target")
-def from_import(p):
-    result = {"type": "from_import", "targets": p[3], "after_space": ""}
-    result.update(p[1])
+def from_import((from_, from_import_module, import_, from_import_target)):
+    result = {"type": "from_import", "targets": from_import_target, "after_space": ""}
+    result.update(from_import_module)
     return result
 
 @pg.production("from_import : FROM from_import_module IMPORT SPACE from_import_target")
-def from_import_with_space(p):
-    result = {"type": "from_import", "targets": p[4], "after_space": p[3].value}
-    result.update(p[1])
+def from_import_with_space((from_, from_import_module, import_, space, from_import_target)):
+    result = {"type": "from_import", "targets": from_import_target, "after_space": space.value}
+    result.update(from_import_module)
     return result
 
 @pg.production("from_import_module : SPACE dotted_name SPACE")
-def from_import_module(p):
-    return {"before_space": p[0].value, "middle_space": p[2].value, "value": {"type": "dotted_name", "value": p[1]}}
+def from_import_module((space, dotted_name, space2)):
+    return {"before_space": space.value, "middle_space": space2.value, "value": {"type": "dotted_name", "value": dotted_name}}
 
 @pg.production("from_import_module : dotted_name SPACE")
-def from_import_module_no_before_space(p):
-    return {"before_space": "", "middle_space": p[1].value, "value": {"type": "dotted_name", "value": p[0]}}
+def from_import_module_no_before_space((dotted_name, space)):
+    return {"before_space": "", "middle_space": space.value, "value": {"type": "dotted_name", "value": dotted_name}}
 
 @pg.production("from_import_module : SPACE dotted_name")
-def from_import_module_no_after_space(p):
-    return {"before_space": p[0].value, "middle_space": "", "value": {"type": "dotted_name", "value": p[1]}}
+def from_import_module_no_after_space((space, dotted_name)):
+    return {"before_space": space.value, "middle_space": "", "value": {"type": "dotted_name", "value": dotted_name}}
 
 @pg.production("from_import_module : dotted_name")
-def from_import_module_no_spaces(p):
-    return {"before_space": "", "middle_space": "", "value": {"type": "dotted_name", "value": p[0]}}
+def from_import_module_no_spaces((dotted_name,)):
+    return {"before_space": "", "middle_space": "", "value": {"type": "dotted_name", "value": dotted_name}}
 
 @pg.production("from_import_target : name_as_names")
-def from_import_target_name_as_names(p):
-    return p[0]
+def from_import_target_name_as_names((name_as_names,)):
+    return name_as_names
 
 @pg.production("from_import_target : LEFT_PARENTHESIS name_as_names RIGHT_PARENTHESIS")
-def from_import_parenthesis(p):
-    return [{"type": "left_parenthesis", "value": "("}] + p[1] + [{"type": "right_parenthesis", "value": ")"}]
+def from_import_parenthesis((left_parenthesis, name_as_names, right_parenthesis)):
+    return [{"type": "left_parenthesis", "value": "("}] + name_as_names + [{"type": "right_parenthesis", "value": ")"}]
 
 @pg.production("from_import_target : STAR")
-def from_import_star(p):
+def from_import_star((star,)):
     return [{"type": "star", "value": "*"}]
 
 @pg.production("name_as_names : name_as_names name_as_name")
-def name_as_names_name_as_name(p):
-    return p[0] + p[1]
+def name_as_names_name_as_name((name_as_names, name_as_name)):
+    return name_as_names + name_as_name
 
 @pg.production("name_as_names : name_as_name")
-def name_as_names(p):
-    return p[0]
+def name_as_names((name_as_name,)):
+    return name_as_name
 
 @pg.production("name_as_name : NAME SPACE AS SPACE NAME")
-def name_as_name_name_as_name(p):
-    return [{"type": "name_as_name", "value": p[0].value, "before_space": p[1].value, "after_space": p[3].value, "as_": True, "target": p[4].value}]
+def name_as_name_name_as_name((name, space, as_, space2, name2)):
+    return [{"type": "name_as_name", "value": name.value, "before_space": space.value, "after_space": space2.value, "as_": True, "target": name2.value}]
 
 @pg.production("name_as_name : NAME")
-def name_as_name_name(p):
-    return [{"type": "name_as_name", "value": p[0].value, "before_space": "", "after_space": ""}]
+def name_as_name_name((name,)):
+    return [{"type": "name_as_name", "value": name.value, "before_space": "", "after_space": ""}]
 
 @pg.production("name_as_name : COMMA")
 @pg.production("name_as_name : SPACE")
-def name_as_name_comma_space(p):
-    return [create_node_from_token(p[0])]
+def name_as_name_comma_space((name_as_name,)):
+    return [create_node_from_token(name_as_name)]
 
 @pg.production("dotted_as_names : dotted_as_names COMMA SPACE dotted_as_name")
-def dotted_as_names_dotted_as_names_dotted_as_name(p):
-    return p[0] + [{"type": "comma", "value": ","}, {"type": "space", "value": " "}] + p[3]
+def dotted_as_names_dotted_as_names_dotted_as_name((dotted_as_names, comma, space, dotted_as_names2)):
+    return dotted_as_names + [{"type": "comma", "value": ","}, {"type": "space", "value": " "}] + dotted_as_names2
 
 @pg.production("dotted_as_names : dotted_as_name")
-def dotted_as_names_dotted_as_name(p):
-    return p[0]
+def dotted_as_names_dotted_as_name((dotted_as_name,)):
+    return dotted_as_name
 
 @pg.production("dotted_as_name : dotted_name SPACE AS SPACE NAME")
-def dotted_as_name_as(p):
-    return [{"type": "dotted_as_name", "value": {"type": "dotted_name", "value": p[0]}, "before_space": p[1].value, "after_space": p[3].value, "target": p[4].value, "as_": True}]
+def dotted_as_name_as((dotted_as_name, space, as_, space2, name)):
+    return [{"type": "dotted_as_name", "value": {"type": "dotted_name", "value": dotted_as_name}, "before_space": space.value, "after_space": space2.value, "target": name.value, "as_": True}]
 
 @pg.production("dotted_as_name : dotted_name")
-def dotted_as_name(p):
-    return [{"type": "dotted_as_name", "value": {"type": "dotted_name", "value": p[0]}, "before_space": "", "after_space": ""}]
+def dotted_as_name((dotted_name,)):
+    return [{"type": "dotted_as_name", "value": {"type": "dotted_name", "value": dotted_name}, "before_space": "", "after_space": ""}]
 
 @pg.production("dotted_name : dotted_name dotted_name_element")
-def dotted_name_elements_element(p):
-    return p[0] + p[1]
+def dotted_name_elements_element((dotted_name, dotted_name_element)):
+    return dotted_name + dotted_name_element
 
 @pg.production("dotted_name : dotted_name_element")
-def dotted_name_element(p):
-    return p[0]
+def dotted_name_element((dotted_name_element,)):
+    return dotted_name_element
 
 @pg.production("dotted_name_element : NAME")
 @pg.production("dotted_name_element : DOT")
 @pg.production("dotted_name_element : SPACE")
-def dotted_name(p):
-    return [create_node_from_token(p[0])]
+def dotted_name((token,)):
+    return [create_node_from_token(token)]
 
 
 parser = pg.build()
