c05c162ccf6de5d63c84e4bac0071433b1a526c8
Laurent Peuch <cortex@worlddomination.be>
2013-07-27 18:18:35 +0200

    [enh] test_name test_string for test space grouper

--- baron/test_space_grouper.py
+++ baron/test_space_grouper.py
@@ -13,3 +13,20 @@ def test_int():
     "1"
     group([('INT', '1')],
           [('INT', '1')])
+
+
+def test_name():
+    "a"
+    group([('NAME', 'a')],
+          [('NAME', 'a')])
+
+
+def test_string():
+    '''
+    "pouet pouet"
+    """pouet pouet"""
+    '''
+    group([('STRING', '"pouet pouet"')],
+          [('STRING', '"pouet pouet"')])
+    group([('STRING', '"""pouet pouet"""')],
+          [('STRING', '"""pouet pouet"""')])
