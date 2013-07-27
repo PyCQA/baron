28432d1c9fb2877dd2bf92eb277d1ecdcf228d13
Laurent Peuch <cortex@worlddomination.be>
2013-07-27 18:59:07 +0200

    [mod] small refactoring

--- baron/space_grouper.py
+++ baron/space_grouper.py
@@ -1,5 +1,19 @@
 from utils import FlexibleIterator
 
+BOTH = (
+    "AS",
+    "IMPORT",
+)
+
+GROUP_SPACE_BEFORE = BOTH + (
+    "DOT",
+    "RIGHT_PARENTHESIS",
+)
+
+GROUP_SPACE_AFTER = BOTH + (
+    "COMMA",
+    "FROM",
+)
 
 def group(sequence):
     return list(group_generator(sequence))
@@ -14,11 +28,11 @@ def group_generator(sequence):
 
         current = iterator.next()
 
-        if current[0] == "SPACE" and iterator.show_next()[0] in ("DOT", "AS", "IMPORT", "RIGHT_PARENTHESIS"):
+        if current[0] == "SPACE" and iterator.show_next()[0] in GROUP_SPACE_BEFORE:
             new_current = iterator.next()
             current = (new_current[0], new_current[1], current[1])
 
-        if current[0] in ("IMPORT", "AS", "COMMA", "FROM", ) and iterator.show_next()[0] == "SPACE":
+        if current[0] in GROUP_SPACE_AFTER and iterator.show_next()[0] == "SPACE":
             _, space_value = iterator.next()
             current = (current[0], current[1], current[2] if len(current) > 2 else '', space_value)
 
