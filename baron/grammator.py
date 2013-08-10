5d99f7b9d20a4603187d4c8029ecca04ef819425
Laurent Peuch <cortex@worlddomination.be>
2013-08-11 01:00:10 +0200

    [mod] rename function to have a more close to reality name

--- baron/grammator.py
+++ baron/grammator.py
@@ -925,7 +925,7 @@ def ternary_operator_node((first, if_, second, else_, third)):
 
 @pg.production("or_test : not_test OR or_test")
 @pg.production("and_test : not_test AND and_test")
-def and_node((first, operator, second)):
+def and_or_node((first, operator, second)):
     return boolean_operator(
         operator.value,
         first=first,
