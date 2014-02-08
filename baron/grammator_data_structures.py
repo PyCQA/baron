e08f14da8c56ee58119aa11d12986e36ff2a3ae3
Laurent Peuch <cortex@worlddomination.be>
2014-02-08 12:54:39 +0100

    [mod] refactor tuple, handle empty case in testlist_comp

--- baron/grammator_data_structures.py
+++ baron/grammator_data_structures.py
@@ -1,16 +1,6 @@
 def include_data_structures(pg):
-    @pg.production("atom : LEFT_PARENTHESIS RIGHT_PARENTHESIS")
-    def tuple((left_parenthesis, right_parenthesis,)):
-        return {
-                "type": "tuple",
-                "first_space": left_parenthesis.after_space,
-                "second_space": "",
-                "value": []
-               }
-
-
     @pg.production("atom : LEFT_PARENTHESIS testlist_comp RIGHT_PARENTHESIS")
-    def tuple_one((left_parenthesis, testlist_comp, right_parenthesis,)):
+    def tuple((left_parenthesis, testlist_comp, right_parenthesis,)):
         return {
                 "type": "tuple",
                 "first_space": left_parenthesis.after_space,
@@ -19,9 +9,9 @@ def include_data_structures(pg):
                }
 
 
-    @pg.production("testlist_comp : test COMMA")
-    def testlist_comp((test, comma)):
-        return [test, {"type": "comma", "value": ",", "first_space": comma.before_space, "second_space": comma.after_space}]
+    @pg.production("testlist_comp :")
+    def testlist_comp_empty(empty):
+        return []
 
 
     @pg.production("testlist_comp : test COMMA test")
