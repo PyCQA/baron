34a82fbbadea2cbd31fb53483ea7f5a51865ce0b
Laurent Peuch <cortex@worlddomination.be>
2014-02-16 00:55:18 +0100

    [fix] only diff grammar when assert failed

--- baron/test_loop.sh
+++ baron/test_loop.sh
@@ -3,14 +3,12 @@
 while true
 do
     clear
-    py.test
-    if [ "$?" == "1" ]
+    py.test -x | tee /tmp/.baron_test_ouput
+    if [ "$(grep AssertionError /tmp/.baron_test_ouput)" ]
     then
         sed 's/, *$//' /tmp/a > /tmp/aa
         sed 's/, *$//' /tmp/b > /tmp/bb
         colordiff -W $(stty size | cut -d " " -f 2) -y /tmp/aa /tmp/bb
-    else
-        echo "ok"
     fi
     inotifywait -e modify *.py
 done
