f87c5ea80d281d9006c28d0c5e7c92044d426357
Laurent Peuch <cortex@worlddomination.be>
2014-02-15 23:09:58 +0100

    [enh] better debug output \o/

--- baron/conftest.py
+++ baron/conftest.py
@@ -1,51 +1,8 @@
 import json
 
-def flaten(jayzon):
-    to_return = []
-    for key, value in jayzon.items():
-        if isinstance(value, basestring):
-            to_return.append('{"%s": "%s", ..}' % (key, value))
-        if isinstance(value, list):
-            to_return.append('{"%s": %s, ..}' % (key, value))
-        if isinstance(value, dict):
-            for result in flaten(value):
-                to_return.append('{"%s": %s, ..}' % (key, result))
-    return to_return
-
-def dict_diff(first, second):
-    first = flaten(first)
-    second = flaten(second)
-    diffs = []
-    for item in first:
-        if item not in second:
-            diffs.append("bad: %s" % item)
-    for item in second:
-        if item not in first:
-            diffs.append("expected: %s" % item)
-
-    return diffs
-
-def diff(left, right):
-    to_return = json.dumps(left, indent=4, sort_keys=True).split("\n")
-    to_return += [""]
-    to_return += ["=="]
-    to_return += [""]
-    to_return += json.dumps(right, indent=4, sort_keys=True).split("\n")
-    to_return += [""]
-    to_return += [`left`, "==", `right`]
-    to_return += [""]
-    with open("/tmp/a", "w") as a:
-        a.write(json.dumps(left, indent=4, sort_keys=True))
-    with open("/tmp/b", "w") as b:
-        b.write(json.dumps(right, indent=4, sort_keys=True))
-    a = 0
-    for first, second in zip(left, right):
-        if first == second:
-            continue
-        to_return += map(lambda x: ("[%s] " % a) + x, dict_diff(first, second))
-        a += 1
-    return to_return
-
 def pytest_assertrepr_compare(config, op, left, right):
     if isinstance(left, list) and isinstance(right, list) and op == "==":
-        return diff(left, right)
+        with open("/tmp/a", "w") as a:
+            a.write(json.dumps(left, indent=4, sort_keys=True))
+        with open("/tmp/b", "w") as b:
+            b.write(json.dumps(right, indent=4, sort_keys=True))
--- baron/test_loop.sh
+++ baron/test_loop.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-while true; do clear; py.test; inotifywait -e modify *.py; done
+while true
+do
+    clear
+    py.test
+    if [ "$?" == "1" ]
+    then
+        colordiff -W $(stty size | cut -d " " -f 2) -y /tmp/a /tmp/b
+    fi
+    inotifywait -e modify *.py
+done
