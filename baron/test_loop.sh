df26d337ba53943405cf9d339c5f796679a0b858
Laurent Peuch <cortex@worlddomination.be>
2014-02-16 00:14:10 +0100

    [mod] dirty tric for cleaner debug diff

--- baron/test_loop.sh
+++ baron/test_loop.sh
@@ -6,7 +6,11 @@ do
     py.test
     if [ "$?" == "1" ]
     then
-        colordiff -W $(stty size | cut -d " " -f 2) -y /tmp/a /tmp/b
+        sed 's/, *$//' /tmp/a > /tmp/aa
+        sed 's/, *$//' /tmp/b > /tmp/bb
+        colordiff -W $(stty size | cut -d " " -f 2) -y /tmp/aa /tmp/bb
+    else
+        echo "ok"
     fi
     inotifywait -e modify *.py
 done
