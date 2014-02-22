ad6cf4d9e4f74ea6cfbf8275f1fdfd3dc068da23
Laurent Peuch <cortex@worlddomination.be>
2014-02-22 15:31:19 +0100

    [enh] display the bugging function in the customoutput test log

--- baron/test_loop.sh
+++ baron/test_loop.sh
@@ -6,6 +6,7 @@ do
     py.test -x | tee /tmp/.baron_test_ouput
     if [ "$(grep ': AssertionError' /tmp/.baron_test_ouput)" ]
     then
+        grep "   def" /tmp/.baron_test_ouput | head -n 1 | sed 's/^ *//'
         sed 's/, *$//' /tmp/a > /tmp/aa
         sed 's/, *$//' /tmp/b > /tmp/bb
         colordiff -W $(stty size | cut -d " " -f 2) -y /tmp/aa /tmp/bb
