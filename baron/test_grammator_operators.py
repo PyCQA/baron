29c1c839a777f71966991157fea068d632ebb778
Laurent Peuch <cortex@worlddomination.be>
2014-02-22 21:27:05 +0100

    [enh] handle slice with lower upper and both :

--- baron/test_grammator_operators.py
+++ baron/test_grammator_operators.py
@@ -2631,6 +2631,46 @@ def test_subscript_slice_lower_upper():
             }]
            }])
 
+def test_subscript_slice_lower_upper_both():
+    "a[b : c :]"
+    parse_simple([
+           ('NAME', 'a'),
+           ('LEFT_SQUARE_BRACKET', '['),
+           ('NAME', 'b'),
+           ('COLON', ':', ' ', ' '),
+           ('NAME', 'c'),
+           ('COLON', ':', ' ', ''),
+           ('RIGHT_SQUARE_BRACKET', ']'),
+          ],
+          [{
+            "type": "atomtrailers",
+            "value": [{
+               "type": "name",
+               "value": "a",
+            },{
+               "type": "getitem",
+               "first_space": "",
+               "second_space": "",
+               "value": {
+                   "type": "slice",
+                   "lower": {
+                        "type": "name",
+                        "value": "b",
+                   },
+                   "upper": {
+                        "type": "name",
+                        "value": "c",
+                   },
+                   "step": None,
+                   "has_two_colons": True,
+                   "first_space": " ",
+                   "second_space": " ",
+                   "third_space": " ",
+                   "forth_space": "",
+               }
+            }]
+           }])
+
 ### trailer: '.' [SPACE] NAME
 ### trailer: '[' [SPACE] ']'
 ### trailer: '[' [SPACE] subscriptlist [SPACE] ']'
