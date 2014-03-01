047adb2082ac901409c7c8048b1893ec38bf7d62
Laurent Peuch <cortex@worlddomination.be>
2014-03-01 15:27:52 +0100

    [enh] add a dummy test

--- baron/test_inner_formatting_grouper.py
+++ baron/test_inner_formatting_grouper.py
@@ -5,3 +5,15 @@ from inner_formatting_grouper import group
 
 def test_empty():
     assert group([]) == []
+
+
+def test_some_stuff():
+    assert group([
+        ('INT', '1'),
+        ('PLUS', '+', ' ', ' '),
+        ('INT', '2')
+    ]) == [
+        ('INT', '1'),
+        ('PLUS', '+', ' ', ' '),
+        ('INT', '2')
+    ]
