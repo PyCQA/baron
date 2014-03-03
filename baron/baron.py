e26f4f6cee83c4bc25bed72b4e493382651eca53
Laurent Peuch <cortex@worlddomination.be>
2014-03-03 06:21:39 +0100

    [mod] don't group space anymore as a separate formatting artefact

--- baron/baron.py
+++ baron/baron.py
@@ -1,7 +1,6 @@
 from spliter import split
 from grouper import group
 from tokenizer import tokenize as _tokenize
-from space_grouper import group as space_group
 from grammator import parser, Token
 from indentation_marker  import mark_indentation
 from inner_formatting_grouper import group as inner_group
@@ -14,8 +13,8 @@ def _parse(tokens):
 def parse(pouet):
     if pouet and pouet[-1] != "\n":
         pouet += "\n"
-    return _parse(inner_group(mark_indentation(space_group(_tokenize(group(split(pouet)))))))
+    return _parse(inner_group(mark_indentation(_tokenize(group(split(pouet))))))
 
 
 def tokenize(pouet):
-    return inner_group(mark_indentation(space_group(_tokenize(group(split(pouet))))))
+    return inner_group(mark_indentation(_tokenize(group(split(pouet)))))
