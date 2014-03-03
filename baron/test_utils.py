c67d2184a987d9b9f160d268057e1aa47fcb17ab
Laurent Peuch <cortex@worlddomination.be>
2014-03-03 07:11:23 +0100

    autopep8

--- baron/test_utils.py
+++ baron/test_utils.py
@@ -1,9 +1,11 @@
 from grammator import parser, Token
 
+
 def parse_simple(tokens, result):
     if not tokens or tokens[-1][0] != "ENDL":
         tokens += [('ENDL', '\n')]
     assert parser.parse(iter(map(lambda x: Token(*x) if x else x, tokens + [('ENDMARKER', ''), None]))) == (result + [{"type": "endl", "value": "\n", "space": "", "indent": ""}])
+
 
 def parse_multi(tokens, result):
     assert parser.parse(iter(map(lambda x: Token(*x) if x else x, tokens + [('ENDMARKER', ''), None]))) == result
