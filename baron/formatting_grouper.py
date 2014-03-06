b4eef91d7dc22afc4420177a15b3bb4f33b03d80
Laurent Peuch <cortex@worlddomination.be>
2014-03-06 18:54:21 +0100

    [mod] remove error handling from formatting_grouper, this is not the place to do that anymore

--- baron/formatting_grouper.py
+++ baron/formatting_grouper.py
@@ -101,41 +101,23 @@ def group(sequence):
 def group_generator(sequence):
     iterator = FlexibleIterator(sequence)
     current = None, None
-    debug_file_content = ""
-    debug_previous_token = None
     while True:
         if iterator.end():
             return
 
-        debug_previous_token = current
         current = iterator.next()
 
         if current is None:
             return
 
-        debug_file_content += current[1]
-
         if current[0] == "SPACE" and iterator.show_next()[0] in GROUP_SPACE_BEFORE:
-            debug_previous_token = current
             new_current = iterator.next()
-            debug_file_content += new_current[1]
             current = (new_current[0], new_current[1], current[1])
 
         if current[0] in GROUP_SPACE_AFTER and\
             (iterator.show_next() and iterator.show_next()[0] == "SPACE") and\
             (iterator.show_next(2) and not less_prioritary_than(current[0], iterator.show_next(2)[0])):
-            debug_previous_token = current
             _, space_value = iterator.next()
-            debug_file_content += space_value
             current = (current[0], current[1], current[2] if len(current) > 2 else '', space_value)
 
-        if current[0] == "SPACE":
-            debug_file_content = debug_file_content.split("\n")
-            debug_file_content = zip(range(1, len(debug_file_content) + 1), debug_file_content)
-            debug_file_content = debug_file_content[-3:]
-            debug_file_content = "\n".join(map(lambda x: "%4s %s" % (x[0], x[1]), debug_file_content))
-            debug_file_content += "<--- here"
-            debug_text = "Unexpected space token:\n\n" + debug_file_content + "\n\n"
-            debug_text += "Should have been grouped on either %s (before) or %s (after) token." % (debug_previous_token, iterator.show_next())
-            raise UnExpectedSpaceToken(debug_text)
         yield current
