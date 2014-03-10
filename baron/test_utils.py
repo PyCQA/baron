f26641a5a495886ecf586c6e902eafca1f6a9550
Laurent Peuch <cortex@worlddomination.be>
2014-03-10 01:58:17 +0100

    [mod] make check_dumps more robust

--- baron/test_utils.py
+++ baron/test_utils.py
@@ -17,6 +17,16 @@ def parse_multi(tokens, result):
 
 
 def check_dumps(source_code):
-    open("/tmp/c", "w").write(source_code)
-    open("/tmp/d", "w").write(dumps(baron_parse(source_code)))
+    try:
+        open("/tmp/c", "w").write(source_code)
+        open("/tmp/d", "w").write(dumps(baron_parse(source_code)))
+    except Exception as e:
+        import sys
+        import json
+        import traceback
+        traceback.print_exc(file=sys.stdout)
+        print "Warning: couldn't write dumps output to debug file, exception: %s" % e
+        print
+        print "Tree: %s" % json.dumps(baron_parse(source_code), indent=4)
+
     assert dumps(baron_parse(source_code)) == source_code
