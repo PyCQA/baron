98f0e9b3c3925c36b94e9c92118f5e60a51fea08
Laurent Peuch <cortex@worlddomination.be>
2014-03-10 06:45:34 +0100

    [mod] refactor, use decorator to populate global dict

--- baron/dumper.py
+++ baron/dumper.py
@@ -3,6 +3,19 @@ def debug(j):
     print json.dumps(j, indent=4)
 
 
+dumpers = {}
+
+
+def node(key=""):
+    def wrap(func):
+        if not key:
+            dumpers[func.__name__ if not func.__name__.endswith("_") else func.__name__[:-1]] = func
+
+        dumpers[key] = func
+        return func
+    return wrap
+
+
 def dump_node(node):
     return "".join(list(dumpers[node["type"]](node)))
 
@@ -11,20 +24,33 @@ def dump_node_list(node_list):
     return "".join(map(dump_node, node_list))
 
 
+@node()
 def endl(node):
     yield dump_node_list(node["formatting"])
     yield node["value"]
     yield node["indent"]
 
 
+@node("name")
+@node("int")
+@node("dot")
+@node("space")
 def get_value(node):
     yield node["value"]
 
 
+@node()
+def pass_(node):
+    return "pass"
+
+
+@node("dotted_name")
+@node("ifelseblock")
 def dump_node_list_value(node):
     yield dump_node_list(node["value"])
 
 
+@node()
 def assignment(node):
     yield dump_node(node["target"])
     yield dump_node_list(node["first_formatting"])
@@ -33,6 +59,7 @@ def assignment(node):
     yield dump_node(node["value"])
 
 
+@node()
 def binary_operator(node):
     yield dump_node(node["first"])
     yield dump_node_list(node["first_formatting"])
@@ -41,6 +68,7 @@ def binary_operator(node):
     yield dump_node(node["second"])
 
 
+@node()
 def while_(node):
     yield "while"
     yield dump_node_list(node["first_formatting"])
@@ -53,6 +81,7 @@ def while_(node):
         yield dump_node(node["else"])
 
 
+@node()
 def if_(node):
     yield "if"
     yield dump_node_list(node["first_formatting"])
@@ -63,6 +92,7 @@ def if_(node):
     yield dump_node_list(node["value"])
 
 
+@node()
 def elif_(node):
     yield "elif"
     yield dump_node_list(node["first_formatting"])
@@ -73,6 +103,7 @@ def elif_(node):
     yield dump_node_list(node["value"])
 
 
+@node()
 def else_(node):
     yield "else"
     yield dump_node_list(node["first_formatting"])
@@ -81,12 +112,14 @@ def else_(node):
     yield dump_node_list(node["value"])
 
 
+@node()
 def import_(node):
     yield "import"
     yield dump_node_list(node["formatting"])
     yield dump_node_list(node["value"])
 
 
+@node()
 def from_import(node):
     yield "from"
     yield dump_node_list(node["first_formatting"])
@@ -97,6 +130,7 @@ def from_import(node):
     yield dump_node_list(node["targets"])
 
 
+@node()
 def dotted_as_name(node):
     yield dump_node_list(node["value"]["value"])
     if node["as"]:
@@ -106,6 +140,7 @@ def dotted_as_name(node):
         yield node["target"]
 
 
+@node()
 def name_as_name(node):
     yield node["value"]
     if node["as"]:
@@ -113,29 +148,6 @@ def name_as_name(node):
         yield "as"
         yield dump_node_list(node["second_formatting"])
         yield node["target"]
-    debug(node)
-
-
-dumpers = {
-    "name": get_value,
-    "dot": get_value,
-    "endl": endl,
-    "int": get_value,
-    "pass": lambda x: "pass",
-    "space": get_value,
-    "assignment": assignment,
-    "binary_operator": binary_operator,
-    "while": while_,
-    "ifelseblock": dump_node_list_value,
-    "if": if_,
-    "elif": elif_,
-    "else": else_,
-    "import": import_,
-    "from_import": from_import,
-    "dotted_as_name": dotted_as_name,
-    "dotted_name": dump_node_list_value,
-    "name_as_name": name_as_name,
-}
 
 
 def dumps(tree):
