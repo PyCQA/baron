729af5d6b3adb97c47b0350a8a38716136987092
Laurent Peuch <cortex@worlddomination.be>
2014-03-10 01:28:53 +0100

    [mod] move test_utils to new behavior

--- baron/test_utils.py
+++ baron/test_utils.py
@@ -1,11 +1,14 @@
-from grammator import parser, Token
+from grammator import generate_parse
+
+
+parse = generate_parse(False)
 
 
 def parse_simple(tokens, result):
     if not tokens or tokens[-1][0] != "ENDL":
         tokens += [('ENDL', '\n')]
-    assert parser.parse(iter(map(lambda x: Token(*x) if x else x, tokens + [('ENDMARKER', ''), None]))) == (result + [{"type": "endl", "value": "\n", "formatting": [], "indent": ""}])
+    assert parse(tokens + [('ENDMARKER', ''), None]) == (result + [{"type": "endl", "value": "\n", "formatting": [], "indent": ""}])
 
 
 def parse_multi(tokens, result):
-    assert parser.parse(iter(map(lambda x: Token(*x) if x else x, tokens + [('ENDMARKER', ''), None]))) == result
+    assert parse(tokens + [('ENDMARKER', ''), None]) == result
