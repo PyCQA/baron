752b7cacfb1d20f57108141d869521099f20509e
Laurent Peuch <cortex@worlddomination.be>
2014-03-21 02:36:46 +0100

    [enh] add a setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+from setuptools import setup
+
+setup(name='baron',
+      version='0.1',
+      description='Full Syntax Tree for python to make writing refactoring code a realist task',
+      author='Laurent Peuch',
+      long_description=open("README.md", "r").read(),
+      author_email='cortex@worlddomination.be',
+      url='https://github.com/Psycojoker/baron',
+      install_requires=['rply'],
+      packages=['baron'],
+      license='lgplv3+',
+      scripts=[],
+      keywords='ast fst refactoring',
+      )
