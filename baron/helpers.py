81e0c44a859d9091f30d9c152b176e49168a75e5
Pierre Penninckx <ibizapeanut@gmail.com>
2014-05-25 10:51:09 +0200

    [enh] add helper on already parsed node

--- baron/helpers.py
+++ baron/helpers.py
@@ -10,3 +10,7 @@ def show(source_code):
 def show_file(target_file):
     with open(target_file, "r") as source_code:
         sys.stdout.write(json.dumps(parse(source_code.read()), indent=4) + "\n")
+
+
+def show_node(node):
+    sys.stdout.write(json.dumps(node, indent=4) + "\n")
