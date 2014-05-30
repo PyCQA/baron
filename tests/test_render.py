48809c2bf99cf83aca10b002e0205690eff3b3fa
Pierre Penninckx <pierre.penninckx@spacebel.be>
2014-05-30 11:21:55 +0200

    [mod] removed unused complexity in test_render

--- tests/test_render.py
+++ tests/test_render.py
@@ -4,19 +4,16 @@ from baron.render import node_types, nodes_rendering_order
 
 @pytest.fixture(params=nodes_rendering_order.keys())
 def dictionnary_node(request):
-    return {
-        'key': request.param,
-        'value': nodes_rendering_order[request.param]
-    }
+    return nodes_rendering_order[request.param]
 
 
 def test_dictionnary_key_validity(dictionnary_node):
-    for key_type, render_key, dependent in dictionnary_node['value']:
+    for key_type, render_key, dependent in dictionnary_node:
         assert key_type in node_types
 
 
 def test_dictionnary_dependent_validity(dictionnary_node):
-    for key_type, render_key, dependent in dictionnary_node['value']:
+    for key_type, render_key, dependent in dictionnary_node:
         assert dependent == True \
             or isinstance(dependent, str) \
             or (isinstance(dependent, list) and all([isinstance(d, str) for d in dependent]))
