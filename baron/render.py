8cdb631ecd8c8429574799b10d8a3c56c15aabcd
Pierre Penninckx <pierre.penninckx@spacebel.be>
2014-06-04 17:33:07 +0200

    [doc] enhance render and RenderWalker documentation

--- baron/render.py
+++ baron/render.py
@@ -2,6 +2,38 @@ from .utils import string_instance
 
 
 def render(node):
+    """Recipe to render a given FST node
+
+    The FST is composed of branch nodes which are either lists or dicts
+    and of leaf nodes which are strings. Branch nodes can have other
+    list, dict or leaf nodes as childs.
+    
+    To render a string, simply output it. To render a list, render each
+    of its elements in order. To render a dict, you must follow the
+    node's entry in the nodes_rendering_order dictionnary and its
+    dependents constraints.
+
+    This function hides all this algorithmic complexity by returning
+    a structured rendering recipe, whatever the type of node. But even
+    better, you should subclass the RenderWalker which simplifies
+    drastically working with the rendered FST.
+
+    The recipe is a list of steps, each step correspond to a child and
+    is actually a 4-uple composed of the following fields:
+    * `key_type` is a string determining the type of the child in the
+        second field (`item`) of the tuple. It can be one of:
+      * 'constant': the child is a string
+      * 'node': the child is a dict
+      * 'key': the child is an element of a dict
+      * 'list': the child is a list
+      * 'formatting': the child is a list specialized in formatting
+    * `item` is the child itself: either a string, a dict or a list.
+    * `render_pos` gives the child's index in the recipe list, by taking
+        gaps produced by dependency constraints into account.
+    * `render_key` gives the key used to access this child from the
+        parent node. It's a string if the node is a dict or a number if
+        its a list.
+    """
     if isinstance(node, list):
         return render_list(node)
     elif isinstance(node, dict):
@@ -629,14 +661,15 @@ nodes_rendering_order = {
 
 
 class RenderWalker:
-    '''Inherit me and overload the methods you want
+    """Inherit me and overload the methods you want
     
-    Please pay attention that the arguments "render_pos" and
-    "render_key" are relative to the parent of the node given by the
-    "node" argument. For example, if walking on a list node and
-    before_node(..., 1, 1) is called, 1 means the node was the 2nd
-    element in the parent list node.
-    '''
+    When calling walk on a FST node, this class will traverse all the
+    node's subtree by following the recipe given by the `render`
+    function for the node and recursively for all its childs.  For
+    "node", "key", "list" and "formatting" childs it will call the
+    corresponding `before` and `after` methods and for "constant" childs
+    it will call `on_leaf`.
+    """
     STOP = True
 
     def before_list(self, node, render_pos, render_key):
