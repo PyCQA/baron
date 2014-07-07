33ebb9d4ec9372856af40f1bb0ecc6c6f5deb823
Laurent Peuch <cortex@worlddomination.be>
2014-07-07 07:32:27 +0200

    [mod] space code for more lisibility

--- baron/path.py
+++ baron/path.py
@@ -16,9 +16,12 @@ def path_to_node(tree, path):
     """FST node located at the given path"""
     if path is None:
         return None
+
     node = tree
+
     for key in path:
         node = child_by_key(node, key)
+
     return node
 
 
