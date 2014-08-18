9acbac5f3336ced037ff4c5493aa42dab7db91aa
Pierre Penninckx <pierre.penninckx@spacebel.be>
2014-08-18 10:44:04 +0200

    [mod] publish all exception

--- baron/__init__.py
+++ baron/__init__.py
@@ -1,6 +1,9 @@
 from . import grouper
 from . import spliter
-from .baron import parse, tokenize, ParsingError
+from .baron import parse, tokenize
 from .dumper import dumps
 from .render import nodes_rendering_order
+from .parser import ParsingError
 from .inner_formatting_grouper import GroupingError, UnExpectedFormattingToken
+from .spliter import UntreatedError
+from .utils import BaronError
