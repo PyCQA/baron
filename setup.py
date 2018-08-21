714ecb9aa11c7aad59b718701a4f2d47eaa4c763
Laurent Peuch <cortex@worlddomination.be>
2018-08-21 14:31:56 +0200

    [enh] release 0.7

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 
 
 setup(name='baron',
-      version='0.6.6',
+      version='0.7',
       description='Full Syntax Tree for python to make writing refactoring code a realist task',
       author='Laurent Peuch',
       long_description=read_md("README.md") + "\n\n" + open("CHANGELOG", "r").read(),
