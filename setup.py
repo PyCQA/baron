c2568951dc49ea34aeb4f5c74617b16137ebebd4
Laurent Peuch <cortex@worlddomination.be>
2021-12-08 20:58:37 +0100

    0.10

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 
 setup(name='baron',
-      version='0.9',
+      version='0.10',
       description='Full Syntax Tree for python to make writing refactoring code a realist task',
       author='Laurent Peuch',
       long_description=open("README.md").read() + "\n\n" + open("CHANGELOG", "r").read(),
